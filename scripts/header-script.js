2b90712b5af58fe883c6b74cb0905e135711a605
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2018-11-20 20:08:21 +0200

    real initial commit

---
+++ scripts/header-script.js
@@ -0,0 +1,11 @@
+$(".menu-anchor").hover(function () {
+    $(this).animate({ fontSize: "+=5px" });
+    $(".menu-anchor").css({ color: 'grey' });
+    $(this).css({ color: 'white' });
+
+
+}, function () {
+    $(this).animate({ fontSize: "-=5px" });
+    $(".menu-anchor").css("color", "white");
+    $(this).css({ 'color': 'white' });
+});
---
+++ scripts/script.js
@@ -0,0 +1,89 @@
+$(function () {
+
+    function isValidEmailAddress(emailAddress) {
+        var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+        // alert( pattern.test(emailAddress) );
+        return pattern.test(emailAddress);
+    }
+
+    $("#header").load("templates/common/header.html");
+    $("#footer").load("templates/common/footer.html");
+
+    //EmailContact
+    $('#contact-form').submit(function (e) {
+        let userEmail = $('#email').val();
+        let userText = $('#text').val();
+        e.preventDefault();
+        if (!userEmail || !userText) {
+
+            alert('Please check your entries');
+
+        } else {
+            $.ajax({
+                url: "https://formspree.io/winsomewe.kek37@gmail.com",
+                method: "POST",
+                data: $(this).serialize(),
+                dataType: "json"
+            });
+            $(this).get(0).reset();
+            alert('Message Sent');
+        }
+    });
+
+    //EmailCheck
+    $('#email').keyup(function () {
+        if (!isValidEmailAddress($('#email').val())) {
+            $('#email').css('border', '3px solid red');
+        } else {
+            $('#email').css('border', '3px solid green');
+        }
+    });
+    //TextCheck
+    $('#text').keyup(function () {
+        if ($('#text').val().length > 1000) {
+            $('#text').css('border', '3px solid red');
+        } else {
+            $('#text').css('border', '3px solid green');
+        }
+    });
+
+    //FormSubmit
+    $('.button').on('click', function () {
+        $('#contact-form').submit();
+    });
+    
+    //Animate
+    // $(".menu-anchor").click(function(){
+    //     // $(this).animate({height: "+=20px"});
+    //     console.log('azis')
+    // });
+    // $(".btn2").click(function(){
+    //     $("#p1").animate({height: "-=20px"});
+    // });
+
+});
+
+
+
+
+
+// function carouselFunc() {
+//     //Carousel
+//     $('#projectCarousel').carousel('cycle');
+//
+//
+// }
+
+
+// // Enable Carousel Indicators
+// $(".item").click(function () {
+//     $("#projectCarousel").carousel(1);
+// });
+//
+// // Enable Carousel Controls
+// $('#projectCarousel > a.carousel-control-prev > span.carousel-control-prev-icon').on('click', function () {
+//     $('#projectCarousel').carousel('next')
+// });
+// $(".right").click(function () {
+//     $("#myCarousel").carousel("next");
+// });
